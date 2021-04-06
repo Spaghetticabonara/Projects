d432cf8d9d2c2ac79879f7ebe5366241112b304b
Spaghetticabonara <61241799+Spaghetticabonara@users.noreply.github.com>
2021-04-06 16:45:59 +0700

    test

---
+++ testapp/src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <p>hello world</p>
+  </div>
+
+
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  data: function () {
+    return {
+      items: (function () {
+        var it = [];
+        for (var i = 0; i < 100; i++) it.push(i+1);
+        return it;
+      })()
+    }
+  },
+  methods: {
+    onSearch: function (query, found) {
+      console.log('search', query);
+    },
+    onClear: function (event) {
+      console.log('clear');
+    },
+    onEnable: function (event) {
+      console.log('enable');
+    },
+    onDisable: function (event) {
+      console.log('disable');
+    },
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
