de05b73c4ee3d078fd5d263452f47e474eca177b
Spaghetticabonara <61241799+Spaghetticabonara@users.noreply.github.com>
2021-04-08 16:41:39 +0700

    add

--- testapp/src/App.vue
+++ testapp/src/App.vue
@@ -1,8 +1,33 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <p>hello world</p>
+<!--    <img alt="Vue logo" src="./assets/logo.png">-->
+<!--    <HelloWorld msg="Welcome to Your Vue.js App"/>-->
+<!--    <p>hello world</p>-->
+<!--    <input v-model="searchText">-->
+    <go-top style="background-color: black"></go-top>
+    <p>hello</p>
+    <p>hello</p>
+    <p>hello</p>
+    <p>hello</p>
+    <p>hello</p>
+    <p>hello</p>
+    <p>hello</p>
+    <p>hello</p>
+    <p>hello</p>
+    <p>hello</p>
+    <p>hello</p>
+    <p>hello</p>
+    <p>hello</p>
+    <p>hello</p>
+    <p>hello</p>
+    <p>hello</p>
+    <p>hello</p>
+    <p>hello</p>
+    <p>hello</p>
+    <p>hello</p>
+    <p>hello</p>
+    <p>hello</p>
+    <p>hello</p>
   </div>
 
 
@@ -10,30 +35,36 @@
 
 <script>
 // import HelloWorld from './components/HelloWorld.vue'
-
+import GoTop from '@inotom/vue-go-top';
+// export default {
+//   data: function () {
+//     return {
+//       items: (function () {
+//         var it = [];
+//         for (var i = 0; i < 100; i++) it.push(i+1);
+//         return it;
+//       })()
+//     }
+//   },
+//   methods: {
+//     onSearch: function (query, found) {
+//       console.log('search', query);
+//     },
+//     onClear: function (event) {
+//       console.log('clear');
+//     },
+//     onEnable: function (event) {
+//       console.log('enable');
+//     },
+//     onDisable: function (event) {
+//       console.log('disable');
+//     },
+//   }
+// }
 export default {
-  data: function () {
-    return {
-      items: (function () {
-        var it = [];
-        for (var i = 0; i < 100; i++) it.push(i+1);
-        return it;
-      })()
-    }
-  },
-  methods: {
-    onSearch: function (query, found) {
-      console.log('search', query);
-    },
-    onClear: function (event) {
-      console.log('clear');
-    },
-    onEnable: function (event) {
-      console.log('enable');
-    },
-    onDisable: function (event) {
-      console.log('disable');
-    },
+  components: {
+    GoTop,
+
   }
 }
 </script>
