e0f8913dcda391dd09c5cd7f6b3a3a68e2496cff
Spaghetticabonara <61241799+Spaghetticabonara@users.noreply.github.com>
2022-06-14 10:54:31 +0700

    LIsts and Keys

--- test/helloworld/src/components/NameList.js
+++ test/helloworld/src/components/NameList.js
@@ -24,7 +24,7 @@ function NameList() {
     ]
     // const names = ['Bruce', 'Clark', 'Diana']
     const personList = persons.map(person =>
-        <Person person={person} />
+        <Person key={person.id} person={person} />
     )
     return (
         <div>
