e249d71c6693d6d62c8891cef2d97a3940ef9785
Spaghetticabonara <61241799+Spaghetticabonara@users.noreply.github.com>
2022-06-16 15:38:59 +0700

    Component Updating Lifecycle Methods

--- test/helloworld/src/components/LifecycleA.js
+++ test/helloworld/src/components/LifecycleA.js
@@ -20,11 +20,32 @@ class LifecycleA extends Component {
         console.log('LifecycleA componentDidMount')
     }
 
+    shouldComponentUpdate() {
+        console.log('LifecycleA shouldComponentUpdate')
+        return true
+    }
+
+    getSnapshotBeforeUpdate() {
+        console.log('LifecycleA getSnapshotBeforeUpdate')
+        return null
+    }
+
+    componentDidUpdate() {
+        console.log('LifecycleA componentDidUpdate')
+    }
+
+    changeState = () => {
+        this.setState({
+            name: 'Spaghetticab'
+        })
+    }
+
     render() {
         console.log('LifecycleA render')
         return (
             <div>
-                Lifecycle A
+                <div>Lifecycle A</div>
+                <button onClick={this.changeState}>Change state</button>
                 <LifecycleB />
             </div>
         )
