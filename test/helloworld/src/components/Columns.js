1174332aa69317d3595e322fd05848951eb0e848
Spaghetticabonara <61241799+Spaghetticabonara@users.noreply.github.com>
2022-06-16 15:59:33 +0700

    Fragments

---
+++ test/helloworld/src/components/Columns.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Columns() {
+    const items = []
+
+    return (
+        // <React.Fragment>
+
+        // </React.Fragment>
+        // or <></>
+        <>
+            <td>Name</td>
+            <td>Peach</td>
+        </>
+    )
+}
+
+export default Columns
