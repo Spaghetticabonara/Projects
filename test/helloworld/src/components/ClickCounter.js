a3fb5c847470698e7ab1eab13d837dd6a7654f75
Spaghetticabonara <61241799+Spaghetticabonara@users.noreply.github.com>
2022-06-20 16:33:44 +0700

    Higher Order Component 2

--- test/helloworld/src/components/ClickCounter.js
+++ test/helloworld/src/components/ClickCounter.js
@@ -1,30 +1,19 @@
 import React, { Component } from 'react'
+import withCounter from './withCounter'
 
 class ClickCounter extends Component {
-    constructor(props) {
-        super(props)
-
-        this.state = {
-            count: 0
-        }
-    }
-
-    incrementCount = () => {
-        this.setState(prevState => {
-            return {
-                count: prevState.count + 1
-            }
-        })
-    }
 
     render() {
-        const { count } = this.state
+        const { count, incrementCount } = this.props
+
         return (
             <div>
-                <button onClick={this.incrementCount}>Click {count} times</button>
+                <button onClick={incrementCount}>
+                    {this.props.name} Click {count} times
+                </button>
             </div>
         )
     }
 }
 
-export default ClickCounter
+export default withCounter(ClickCounter)
---
+++ test/helloworld/src/components/withCounter.js
@@ -0,0 +1,35 @@
+import React from 'react'
+
+const withCounter = (WrappedComponent) => {
+    class withCounter extends React.Component {
+        constructor(props) {
+            super(props)
+
+            this.state = {
+                count: 0
+            }
+        }
+
+        incrementCount = () => {
+            this.setState(prevState => {
+                return {
+                    count: prevState.count + 1
+                }
+            })
+        }
+
+        render() {
+            console.log(this.props.name)
+            return (
+                <WrappedComponent
+                    count={this.state.count}
+                    incrementCount={this.incrementCount}
+                    {...this.props}
+                />
+            )
+        }
+    }
+    return withCounter
+}
+
+export default withCounter
