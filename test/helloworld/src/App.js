db84ec6248682e31f762a797d3dde3eca025f367
Spaghetticabonara <61241799+Spaghetticabonara@users.noreply.github.com>
2022-06-23 16:25:57 +0700

    Context

--- test/helloworld/src/App.js
+++ test/helloworld/src/App.js
@@ -1,48 +1,18 @@
 // import logo from './logo.svg';
 import './App.css';
-import Greet from './components/Greet'
-import Welcome from './components/Welcome'
-import Hello from './components/Hello'
-import Message from './components/Message';
-import Counter from './components/Counter';
-import FunctionClick from './components/FunctionClick';
-import ClassClick from './components/ClassClick';
-import EventBind from './components/EventBind';
-import ParentComponent from './components/ParentComponent';
-import UserGreeting from './components/UserGreeting';
-import NameList from './components/NameList';
-import Stylesheet from './components/Stylesheet';
-import Inline from './components/Inline';
 import './appStyles.css'
-import styles from './appStyles.module.css'
-import Form from './components/Form';
-import LifecycleA from './components/LifecycleA';
-import FragmentDemo from './components/FragmentDemo';
-import Table from './components/Table';
-import PureComp from './components/PureComp';
-import ParentComp from './components/ParentComp';
-import RefsDemo from './components/RefsDemo';
-import FocusInput from './components/FocusInput';
-import FRParentInput from './components/FRParentInput';
-import PortalDemo from './components/PortalDemo';
-import Hero from './components/Hero';
-import ErrorBoundary from './components/ErrorBoundary';
-import ClickCounter from './components/ClickCounter';
-import HoverCounter from './components/HoverCounter';
 import ClickCounterTwo from './components/ClickCounterTwo';
 import HoverCounterTwo from './components/HoverCounterTwo';
-import User from './components/User';
 import CounterTwo from './components/CounterTwo';
+import ComponentC from './components/ComponentC';
+import { UserProvider } from './components/userContext';
 
 function App() {
   return (
     <div className="App">
-      <CounterTwo render={(count, incrementCount) =>
-        <ClickCounterTwo count={count} incrementCount={incrementCount} />} />
-
-      <CounterTwo render={(count, incrementCount) =>
-        <HoverCounterTwo count={count} incrementCount={incrementCount} />} />
-      {/* <User render={(isLoggedIn) => isLoggedIn ? 'Peach' : 'Guest'} /> */}
+      <UserProvider value="Peach">
+        <ComponentC />
+      </UserProvider>
     </div>
   );
 }
