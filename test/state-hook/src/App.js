5db5cad61d1dad30e266462b9c114e50fa7dcfd1
Spaghetticabonara <61241799+Spaghetticabonara@users.noreply.github.com>
2022-07-01 14:32:40 +0700

    useEffect Hook, useEffect after render

--- test/state-hook/src/App.js
+++ test/state-hook/src/App.js
@@ -1,7 +1,9 @@
 import './App.css';
 import ClassCounter from './components/ClassCounter';
+import ClassCounterOne from './components/ClassCounterOne';
 import HookCounter from './components/HookCounter';
 import HookCounterFour from './components/HookCounterFour';
+import HookCounterOne from './components/HookCounterOne';
 import HookCounterThree from './components/HookCounterThree';
 import HookCounterTwo from './components/HookCounterTwo';
 
@@ -9,7 +11,8 @@ function App() {
   return (
     <div className="App">
       {/* <ClassCounter /> */}
-      <HookCounterFour />
+      {/* <ClassCounterOne /> */}
+      <HookCounterOne />
     </div>
   );
 }
