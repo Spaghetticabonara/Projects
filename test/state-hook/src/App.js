097224d46580286d46958e8d5f3fd41651acb396
Spaghetticabonara <61241799+Spaghetticabonara@users.noreply.github.com>
2022-07-01 13:34:46 +0700

    useState Hook, with previous state, with object, with array

---
+++ test/state-hook/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import ClassCounter from './components/ClassCounter';
+import HookCounter from './components/HookCounter';
+import HookCounterFour from './components/HookCounterFour';
+import HookCounterThree from './components/HookCounterThree';
+import HookCounterTwo from './components/HookCounterTwo';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <ClassCounter /> */}
+      <HookCounterFour />
+    </div>
+  );
+}
+
+export default App;
