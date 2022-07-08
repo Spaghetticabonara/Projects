18754054110c0c8fcf48e41f1831f8ba45e6a8aa
Spaghetticabonara <61241799+Spaghetticabonara@users.noreply.github.com>
2022-07-08 16:14:03 +0700

    useContext Hook

--- test/state-hook/src/App.js
+++ test/state-hook/src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
+import React from 'react';
 import ClassCounter from './components/ClassCounter';
 import ClassCounterOne from './components/ClassCounterOne';
+import ComponentC from './components/ComponentC';
 import DataFetching from './components/DataFetching';
 import HookCounter from './components/HookCounter';
 import HookCounterFour from './components/HookCounterFour';
@@ -11,12 +13,20 @@ import HookMouse from './components/HookMouse';
 import IntervalHookCounter from './components/IntervalHookCounter';
 import MouseContainer from './components/MouseContainer';
 
+export const UserContext = React.createContext()
+export const ChannelContext = React.createContext()
+
 function App() {
   return (
     <div className="App">
       {/* <ClassCounter /> */}
       {/* <ClassCounterOne /> */}
-      <DataFetching />
+      <UserContext.Provider value={'Peach'}>
+        <ChannelContext.Provider value={'Hello world'}>
+          <ComponentC />
+        </ChannelContext.Provider>
+      </UserContext.Provider>
+
     </div>
   );
 }
