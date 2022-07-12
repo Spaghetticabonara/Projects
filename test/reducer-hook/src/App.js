fb23b7962444ec782d1c244d1b49edb7cff9f1ff
Spaghetticabonara <61241799+Spaghetticabonara@users.noreply.github.com>
2022-07-12 14:29:58 +0700

    useReducer (simple state, complex state & action)

---
+++ test/reducer-hook/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import CounterOne from './components/CounterOne';
+import CounterTwo from './components/CounterTwo';
+
+function App() {
+  return (
+    <div className="App">
+      <CounterTwo />
+    </div>
+  );
+}
+
+export default App;
